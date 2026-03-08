2ddbe19dafe7ee5fbf30f9486696662c29eb3e11
eshwarB-2003 <e8082409@gmail.com>
2026-03-08 16:19:54 +0000

    Installed Dependencies and Structured folders

---
+++ flight_booking_management_backend/FlightInventoryService/src/main/java/com/flightbooking/FlightInventoryService/FlightInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flightbooking.FlightInventoryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightInventoryServiceApplication.class, args);
+	}
+
+}
