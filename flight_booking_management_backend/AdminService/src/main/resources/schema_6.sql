2f6968a6ef06a0a186d927a1261ce4547433fdae
eshwarB-2003 <e8082409@gmail.com>
2026-03-22 23:15:06 +0000

    database and tables are added

---
+++ flight_booking_management_backend/AdminService/src/main/resources/schema_6.sql
@@ -0,0 +1,10 @@
+CREATE TABLE audit_log (
+    log_id SERIAL PRIMARY KEY,
+    user_id INT,
+    action VARCHAR(100),
+    entity_type VARCHAR(50),
+    entity_id INT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    details TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
--- flight_booking_management_backend/UserService/src/main/resources/schema_1.sql
+++ flight_booking_management_backend/UserService/src/main/resources/schema_1.sql
@@ -9,16 +9,6 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE audit_log (
-    log_id SERIAL PRIMARY KEY,
-    user_id INT,
-    action VARCHAR(100),
-    entity_type VARCHAR(50),
-    entity_id INT,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    details TEXT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
 
 ALTER TABLE users
 ADD COLUMN updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
